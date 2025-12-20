bef296ebed3e5db5c0e6ded54575d6e75e602da8
jdobbs34 <dobbsjason34@yahoo.com>
2025-12-19 19:21:12 -0700

    2nd commit

---
+++ scripts/script.js
@@ -0,0 +1,92 @@
+// Menu data structure
+var menuLinks = [
+  {text: 'about', href: '/about'},
+  {text: 'catalog', href: '#', subLinks: [
+    {text: 'all', href: '/catalog/all'},
+    {text: 'top selling', href: '/catalog/top'},
+    {text: 'search', href: '/catalog/search'},
+  ]},
+  {text: 'orders', href: '#' , subLinks: [
+    {text: 'new', href: '/orders/new'},
+    {text: 'pending', href: '/orders/pending'},
+    {text: 'history', href: '/orders/history'},
+  ]},
+  {text: 'account', href: '#', subLinks: [
+    {text: 'profile', href: '/account/profile'},
+    {text: 'sign out', href: '/account/signout'},
+  ]},
+];
+
+
+// Part 1 -------------------------------------------------
+// 1.   Select and cache the <main> element in a variable named mainEl.
+let mainEl = document.querySelector("main");
+
+// 2.   Set the background color of mainEl to the value stored in the --main-bg CSS custom property.
+// Hint: Assign a string that uses the CSS var() function like this: 'var(--main-bg)'.
+mainEl.style.backgroundColor = "var(--main-bg)";
+
+// 3.   Set the content of mainEl to <h1>DOM Manipulation</h1>. There are a variety of ways to do this; pick whichever one that you think works best in this situation.
+mainEl.innerHTML = `<h1>DOM Manipulation</h1>`;
+
+// 4.   Add a class of flex-ctr to mainEl.
+// Hint: Use the Element.classList API.
+mainEl.classList.add("flex-ctr");
+
+// Part 2 -------------------------------------------------
+// 1.   Select and cache the <nav id="top-menu"> element in a variable named topMenuEl.
+const topMenuEl = document.getElementById("top-menu");
+
+// 2.   Set the height of the topMenuEl element to be 100%.
+topMenuEl.style.height = "100%";
+
+// 3.   Set the background color of topMenuEl to the value stored in the --top-menu-bg CSS custom property.
+topMenuEl.style.backgroundColor = "var(--top-menu-bg)";
+
+// 4.   Add a class of flex-around to topMenuEl.
+topMenuEl.classList.add("flex-around");
+// console.log(topMenuEl);
+
+// // Part 3 -----------------------------------------------
+// 1.   Iterate over the entire menuLinks array and for each "link" object:
+for (let link of menuLinks) {
+  // 2.   Create an <a> element.
+  let anchor = document.createElement("a");
+
+  // 3.   On the new element, add an href attribute with its value set to the href property of the "link" object.
+  anchor.setAttribute("href", link.href);
+
+ 
+  // 4.   Set the new element's content to the value of the text property of the "link" object.
+  anchor.textContent = link.text;
+
+  // 5.   Append the new element to the topMenuEl element.
+  topMenuEl.appendChild(anchor);
+}
+
+const subMenuEl = document.getElementById("sub-menu");
+
+subMenuEl.style.height = "100%";
+
+subMenuEl.style.backgroundColor = "var(--sub-menu-bg)";
+
+subMenuEl.classList.add("flex-around");
+
+subMenuEl.style.position = "absolute";
+
+subMenuEl.style.top = "0";
+
+let topMenuLinks = topMenuEl.querySelector("a")
+
+topMenuEl.addEventListener('click', function (event) {
+   event.preventDefault();
+  if (event.target.tag === 'a') {
+        return;
+    }
+    console.log(event.target.textContent)
+});
+
+topMenuLinks.addEventListener('active', function (event) {
+   
+});
+
